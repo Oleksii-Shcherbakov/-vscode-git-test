7c602679080c3a14c3dd938a59b967db95551fa3
Oleksii <65829894+Oleksii-Shcherbakov@users.noreply.github.com>
2023-08-14 12:55:50 +0000

    upd

--- calculator.js
+++ calculator.js
@@ -1,16 +1,9 @@
-//calculator.js
+// calculator.js
 
-// Функция, возводящая в квадрат все элементы входящего массива чисел
-export function getSquaredArray(arr) {
-  return arr.map(num => num ** 2);
-}
-
-// Функция, фильтрующая нечетные числа во входящем массиве
-export function getOddNumbers(arr) {
-  return arr.filter(num => num % 2 !== 0);
-}
-
-// Функция, возвращающая сумму двух чисел
-export default function addNumbers(a, b) {
-  return a + b;
+export function calc(expression) {
+  try {
+    return eval(expression);
+  } catch (error) {
+    return 'Ошибка вычисления';
+  }
 }
--- calculator.test.js
+++ calculator.test.js
@@ -1,23 +1,28 @@
 // calculator.test.js
 
-import { getSquaredArray, getOddNumbers } from './calculator';
-import addNumbers from './calculator';
+import { calc } from './calculator';
 
-test('возводит в квадрат все элементы массива', () => {
-  const inputArray = [1, 2, 3, 4];
-  const expectedOutput = [1, 4, 9, 16];
-  expect(getSquaredArray(inputArray)).toEqual(expectedOutput);
+test('проверка операции сложения', () => {
+  expect(calc('2 + 3')).toBe(5);
 });
 
-test('фильтрует нечетные числа', () => {
-  const inputArray = [1, 2, 3, 4, 5];
-  const expectedOutput = [1, 3, 5];
-  expect(getOddNumbers(inputArray)).toEqual(expectedOutput);
+test('проверка операции вычитания', () => {
+  expect(calc('5 - 3')).toBe(2);
 });
 
-test('возвращает сумму двух чисел', () => {
-  const a = 5;
-  const b = 7;
-  const expectedSum = a + b;
-  expect(addNumbers(a, b)).toBe(expectedSum);
+test('проверка операции умножения', () => {
+  expect(calc('4 * 3')).toBe(12);
 });
+
+test('проверка операции деления', () => {
+  expect(calc('10 / 2')).toBe(5);
+});
+
+test('обработка некорректной строки', () => {
+  expect(calc('abc')).toBe('Ошибка вычисления');
+});
+
+test('обработка передачи не строки', () => {
+  expect(calc(123)).toBe('Ошибка вычисления');
+});
+
